1852409adae99ca5c75a3efdb8aff6c73053e4dd
Paul Ballesty <paulballesty@gmail.com>
2014-02-23 03:52:07 -0300

    Email form ready

--- app/scripts/form.js
+++ app/scripts/form.js
@@ -3,10 +3,19 @@ Motio.controller('FormController', ['$scope', '$resource', function ($scope, $re
   var Contact = $resource('/contact');
 
   $scope.contact = {};
+  $scope.sent = false;
 
   $scope.send = function () {
-    new Contact($scope.contact).$save();
-
+    // new Contact($scope.contact).$save(function () {
+      $scope.contact = {};
+      $scope.contactForm.$setPristine();
+      $scope.sent = true;
+      _.delay(function () {
+        $scope.$apply(function () {
+          $scope.sent = false;
+        });
+      }, 2000);
+    // });
   };
 
 }]);
