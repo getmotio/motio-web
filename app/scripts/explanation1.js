d16bcf8763f9ecea0ec5a11ee2c3263c6d679369
Paul Ballesty <paulballesty@gmail.com>
2014-04-05 15:15:38 -0300

    Removing unused controller

--- app/scripts/explanation1.js
+++
@@ -1,8 +0,0 @@
-Motio.controller('Explanation1Controller', ['$scope', function ($scope) {
-	'use strict';
-
-	$scope.requestDemo = function () {
-		$.scrollTo($('.explanation-4'), 500);
-	};
-
-}]);
